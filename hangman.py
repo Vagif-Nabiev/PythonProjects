abc250e8799c7534d66419813ed2c7ac4862a9f1
vaqif <vagifnbiyev@gmail.com>
2024-10-11 17:25:28 +0300

    Added countries to catalog

--- hangman.py
+++ hangman.py
@@ -9,27 +9,32 @@ df = pd.DataFrame(country_names, columns=["Country Name"])
 correct_letters = []
 
 random_index = random.randint(0, len(country_names) - 1)
-chosen_word = country_names[random_index]
+chosen_word = country_names[random_index].lower()
 print("chosen word: " + chosen_word)
 
 guesses = 0
-while guesses < 7 :
+while guesses < 7:
     placeholder = ""
-    guess = input("Guess a letter: ")
+    guess = input("Guess a letter: ").lower()
 
     if len(guess) != 1 or not guess.isalpha():
-        print("Invalid input")
+        print("Invalid input. Please guess a single letter.")
+        continue
+
+    if guess in correct_letters:
+        print(f"You've already guessed '{guess}'. Try a different letter.")
         continue
 
     if guess in chosen_word:
-        if guess not in correct_letters:
-            correct_letters.append(guess)
+        correct_letters.append(guess)
     else:
         guesses += 1
 
     for letter in chosen_word:
         if letter in correct_letters:
             placeholder += letter
+        elif letter == " ":
+            placeholder += " "
         else:
             placeholder += "_"
 
@@ -37,7 +42,7 @@ while guesses < 7 :
     print(f"Guesses left: {7 - guesses}")
 
     if "_" not in placeholder:
-        print("you won")
+        print("You won!")
         break
 else:
     print("You lost")
