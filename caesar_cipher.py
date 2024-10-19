7d068f61c31a3ab62ef05e121e1ddc7a2f0c41e4
vaqif <vagifnbiyev@gmail.com>
2024-10-19 22:29:03 +0300

    updated method for more efficiency

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,6 +1,17 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt or 'decode' to decrypt:\n ").lower()
-text = input("Type your message:\n")
+while True:
+    direction = input("Type 'encode' to encrypt or 'decode' to decrypt:\n ").lower()
+    if direction in ['encode', 'decode']:
+        break
+    else:
+        print("Invalid input. Please type 'encode' or 'decode' and try again.")
+while True:
+    text = input("Type your message:\n").lower()
+    if all(char in alphabet for char in text):
+        break
+    else:
+        print("Invalid input. Please use only alphabetic characters (a-z) and try again.")
+
 shift = int(input("Type the shift:\n"))
 
 if direction == 'encode':
@@ -24,3 +35,4 @@ elif direction == 'decode':
                 hidden_text += alphabet[shifted_position]
         print('Decoded text: '+hidden_text, end='')
     decrypt(text, shift)
+
